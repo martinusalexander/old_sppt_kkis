940c072473bf8aabfa5ef918afc27ef55d8024fb
Martinus Alexander <martinus.alexander.looho@gmail.com>
2017-09-24 12:24:22 +0800

    Fix wrong email content when password is changed

--- resources/views/user/email/passwordchanged.blade.php
+++ resources/views/user/email/passwordchanged.blade.php
@@ -1,7 +1,7 @@
 <body>
     
     <p>Hi {{ $name }},</p>
-    <p>Anda Kami telah menerima permintaan untuk mengubah password Anda.</p>
+    <p>Kami telah menerima permintaan untuk mengubah password Anda.</p>
     <p>Jika bukan Anda yang melakukan pengubahan ini, mohon hubungan administrator melalui email <a href="mailto:humas.intern@kkis.org?Subject=Permintaan%20Atur%20Ulang%20Password" target="_top">humas.intern@kkis.org</a></p>
     <p>Salam,
     <br>
